1e977bbfbfbbc0e15703270927aa4a91e4c7e52c
tigtrop <fredbios33@gmail.com>
2023-11-09 17:41:45 +0200

    new tests are added

--- api/test_api.py
+++ api/test_api.py
@@ -6,7 +6,23 @@ import pytest
 headers = {'Content-Type': 'application/json', 'User-Agent': 'Python Learning Requests'}
 baseUrl = 'http://api.mathjs.org/v4/'
 
+def postOperation(action, num1, num2):
+    sign = ''
+
+    match action:
+        case 'add':
+            sign = '+'
+        case 'divide':
+            sign = '/'
+        case 'substract':
+            sign = '-'
+        case 'multiplicate':
+            sign = '*'
+
+    return requests.post(baseUrl, headers=headers, json={'expr': [str(num1) + sign + str(num2)]})
+
 # test 1 square
+@pytest.mark.skip
 @pytest.mark.parametrize('number', [1, 2, 3, 4, 5])
 def test_square(number):
     response = requests.get(baseUrl, headers=headers, params={'expr': number**2})
@@ -16,6 +32,24 @@ def test_square(number):
 # test 2 plus
 @pytest.mark.parametrize('number1, number2', [(1, 2), (3, 4), (5, 6), (7, 8), (9, 10)])
 def test_addition(number1, number2):
-    response = requests.post(baseUrl, headers=headers, json={'expr': [str(number1)+ '+' +str(number2)]})
+    response = postOperation('add', number1, number2)
     answear = response.json()['result']
     assert  int(answear[0]) == number1+number2
+
+@pytest.mark.parametrize('number1, number2', [(1, 2), (3, 4), (5, 6), (7, 8), (9, 10)])
+def test_substraction(number1, number2):
+    response = postOperation('substract', number1, number2)
+    answear = response.json()['result']
+    assert  int(answear[0]) == number1-number2
+
+@pytest.mark.parametrize('number1, number2', [(1, 2), (3, 4), (5, 6), (7, 8), (9, 10)])
+def test_division(number1, number2):
+    response = postOperation('divide', number1, number2)
+    answear = response.json()['result']
+    assert  int(answear[0]) == number1/number2
+
+@pytest.mark.parametrize('number1, number2', [(1, 2), (3, 4), (5, 6), (7, 8), (9, 10)])
+def test_multiplication(number1, number2):
+    response = postOperation('multiplicate', number1, number2)
+    answear = response.json()['result']
+    assert  int(answear[0]) == number1*number2
