41a406fc0c4c609b41e3aa2b5afde18c43968984
tigtrop <fredbios33@gmail.com>
2023-11-12 21:26:13 +0200

    test update

--- pages/search_page.py
+++ pages/search_page.py
@@ -1,9 +1,17 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 class SearchPage:
+
+    SEARCH_INPUT = (By.ID, 'searchbox_input')
+
+    URL = 'https://duckduckgo.com/'
     def __init__(self, browser):
         self.browser = browser
 
     def open(self):
-        pass
+        self.browser.get(self.URL)
 
     def search(self, phrase):
-        pass
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        search_input.send_keys(phrase + Keys.RETURN)
--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,5 +1,12 @@
 import pytest
 import selenium.webdriver
 
+from pages.search_page import SearchPage
 def test_basic_search(browser):
-    raise Exception("Bam")
+    search_page = SearchPage(browser)
+
+    PHRASE = 'google'
+
+    search_page.open()
+
+    search_page.search(PHRASE)
