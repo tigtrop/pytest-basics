1305226a9b205d29b02e9b2453c70930ec3dc075
tigtrop <fredbios33@gmail.com>
2023-11-24 22:41:42 +0200

    test is updated

--- pages/form_page.py
+++ pages/form_page.py
@@ -4,17 +4,17 @@ from selenium.webdriver.common.keys import Keys
 class FormPage:
 
     CustomerNameField = (By.CSS_SELECTOR, 'input[name="custname"]')
-    TelephoneField = (By.CSS_SELECTOR, 'input[name="custtel"]')
+    PhoneField = (By.CSS_SELECTOR, 'input[name="custtel"]')
     EmailField = (By.CSS_SELECTOR, 'input[name="custemail"]')
     SmallPizzaRadioButton = (By.CSS_SELECTOR, 'input[value="small"]')
     MediumPizzaRadioButton = (By.CSS_SELECTOR, 'input[value="medium"]')
     LargePizzaRadioButton = (By.CSS_SELECTOR, 'input[value="large"]')
-    PizzaBaconCheckbox = (By.CSS_SELECTOR, 'input[value="bacon"]')
-    PizzaExtraCheeseCheckbox = (By.CSS_SELECTOR, 'input[value="cheese"]')
-    PizzaOnionCheckbox = (By.CSS_SELECTOR, 'input[value="onion"]')
-    PizzaMushroomCheckbox = (By.CSS_SELECTOR, 'input[value="mushroom"]')
+    AddBaconCheckbox = (By.CSS_SELECTOR, 'input[value="bacon"]')
+    AddExtraCheeseCheckbox = (By.CSS_SELECTOR, 'input[value="cheese"]')
+    AddOnionCheckbox = (By.CSS_SELECTOR, 'input[value="onion"]')
+    AddMushroomCheckbox = (By.CSS_SELECTOR, 'input[value="mushroom"]')
     DeliveryTimePicker = (By.CSS_SELECTOR, 'input[name="delivery"]')
-    CommentField = (By.CSS_SELECTOR, 'input[name="comments"]')
+    CommentField = (By.CSS_SELECTOR, 'textarea[name="comments"]')
     SubmitButton = (By.CSS_SELECTOR, 'button')
 
 
@@ -28,3 +28,74 @@ class FormPage:
     def enterCustomerName(self, customerName):
         customerNameField = self.browser.find_element(*self.CustomerNameField)
         customerNameField.send_keys(customerName)
+
+    def enterPhoneNumber(self, phoneNumber):
+        phoneNumberField = self.browser.find_element(*self.PhoneField)
+        phoneNumberField.send_keys(phoneNumber)
+
+    def enterEmail(self, email):
+        emailField = self.browser.find_element(*self.EmailField)
+        emailField.send_keys(email)
+
+    def selectSmallPizza(self):
+        smallPizzaRadio = self.browser.find_element(*self.SmallPizzaRadioButton)
+        smallPizzaRadio.click()
+
+    def selectMediumPizza(self):
+        mediumPizzaRadio = self.browser.find_element(*self.MediumPizzaRadioButton)
+        mediumPizzaRadio.click()
+
+    def selectLargePizza(self):
+        largePizzaRadio = self.browser.find_element(*self.LargePizzaRadioButton)
+        largePizzaRadio.click()
+
+    def addBacon(self):
+        addBacon = self.browser.find_element(*self.AddBaconCheckbox)
+        addBacon.click()
+
+    def addCheese(self):
+        addCheese = self.browser.find_element(*self.AddExtraCheeseCheckbox)
+        addCheese.click()
+
+    def addOnion(self):
+        addOnion = self.browser.find_element(*self.AddOnionCheckbox)
+        addOnion.click()
+
+    def addMushrooms(self):
+        addMushrooms = self.browser.find_element(*self.AddMushroomCheckbox)
+        addMushrooms.click()
+
+    def enterDeliveryTime(self, deliveryTime):
+        deliveryTimeField = self.browser.find_element(*self.DeliveryTimePicker)
+        deliveryTimeField.send_keys(deliveryTime)
+
+    def enterComment(self, comment):
+        commentField = self.browser.find_element(*self.CommentField)
+        commentField.send_keys(comment)
+
+    def clickSublit(self):
+        submitButton = self.browser.find_element(*self.SubmitButton)
+        submitButton.click()
+
+################################################
+
+    def choosePizzaSize(self, size):
+        if size == "Small":
+            self.selectSmallPizza()
+        elif size == "Medium":
+            self.selectMediumPizza()
+        elif size == "Large":
+            self.selectLargePizza()
+        else:
+            raise Exception("The pizza size is not supported")
+
+    def addToppings(self, addBacon, addCheese, addOnion, addMushroom):
+        if addBacon == "Yes":
+            self.addBacon()
+        if addCheese == "Yes":
+            self.addCheese()
+        if addOnion == "Yes":
+            self.addOnion()
+        if addMushroom == "Yes":
+            self.addMushrooms()
+
--- tests/test_pizza_form.py
+++ tests/test_pizza_form.py
@@ -1,15 +1,32 @@
 import pytest
 import selenium.webdriver
 import time
+import json
 
 from pages.form_page import FormPage
-
-def test_form_check(browser):
+@pytest.mark.parametrize('customer_name, phone, email, size, addBacon, addCheese, addOnion, addMushroom, deliveryTime, comment',
+                         [('David', '09933165847', 'mail@test.com', 'Small', 'Yes', 'No', 'No', 'No', '13:00', 'Faster please')])
+def test_form_check(browser, customer_name, phone, email, size, addBacon, addOnion, addCheese, addMushroom, deliveryTime, comment):
     form_page = FormPage(browser)
-
-    customer_name = "Kendro"
 
     form_page.open()
 
     form_page.enterCustomerName(customer_name)
+
+    form_page.enterPhoneNumber(phone)
+
+    form_page.enterEmail(email)
+
+    form_page.choosePizzaSize(size)
+
+    form_page.addToppings(addBacon,addCheese,addOnion,addMushroom)
+
+    form_page.enterDeliveryTime(deliveryTime)
+
+    form_page.enterComment(comment)
+
+    form_page.clickSublit()
+
+
+
     time.sleep(10)
