a17087a6aa39597b494d05add2db2c8012e27869
tigtrop <fredbios33@gmail.com>
2023-11-30 22:44:56 +0200

    selectors for the main page is added

--- pages/db_page.py
+++ pages/db_page.py
@@ -5,22 +5,24 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-
-
 class DB_Page:
 
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
     base_url = 'https://my-json-server.typicode.com/'
     db_endpoint = 'IlyaKnysh/fake_db/db?'
     db_ui = 'IlyaKnysh/fake_db'
     headers = {'accept-encoding': 'gzip, deflate', 'Connection': 'keep-alive', 'cache-control': 'no-cache'}
 
-    # def __init__(self, self.driver):
-    #     self.browser = browser
+    DB_TITLE = (By.CSS_SELECTOR, 'h3 a[href="https://github.com/IlyaKnysh/fake_db"]')
+    DB_CREATOR = (By.CSS_SELECTOR, 'h5 a')
+    DB_INFO_LINK = (By.CSS_SELECTOR, 'a[href="/IlyaKnysh/fake_db/db_info"]')
+    APP_IDS_LINK = (By.CSS_SELECTOR, 'a[href="/IlyaKnysh/fake_db/app_ids"]')
+    AD_ZONES_LINK = (By.CSS_SELECTOR, 'a[href="/IlyaKnysh/fake_db/ad_zones"]')
+    NETWORKS_LINK = (By.CSS_SELECTOR, 'a[href="/IlyaKnysh/fake_db/networks"]')
 
     def open(self):
-        self.driver.get(self.base_url + self.db_ui)
+        self.browser.get(self.base_url + self.db_ui)
     def get_db_structure(self):
         response = requests.get(self.base_url + self.db_endpoint, headers=self.headers)
         return response
