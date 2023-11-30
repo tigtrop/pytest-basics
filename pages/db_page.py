1e1d446f28078ed51c5d409d4c25f0cc5a4a2199
tigtrop <fredbios33@gmail.com>
2023-11-30 22:25:33 +0200

    webdriver manager is added

--- pages/db_page.py
+++ pages/db_page.py
@@ -1,13 +1,27 @@
 import requests
 import pytest
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
 class DB_Page:
 
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
     base_url = 'https://my-json-server.typicode.com/'
-    db = 'IlyaKnysh/fake_db'
+    db_endpoint = 'IlyaKnysh/fake_db/db?'
+    db_ui = 'IlyaKnysh/fake_db'
     headers = {'accept-encoding': 'gzip, deflate', 'Connection': 'keep-alive', 'cache-control': 'no-cache'}
 
-    def get_response(self):
-        response = requests.get(self.base_url + self.db, headers=self.headers)
+    # def __init__(self, self.driver):
+    #     self.browser = browser
+
+    def open(self):
+        self.driver.get(self.base_url + self.db_ui)
+    def get_db_structure(self):
+        response = requests.get(self.base_url + self.db_endpoint, headers=self.headers)
         return response
 
--- tests/test_fake_db.py
+++ tests/test_fake_db.py
@@ -5,7 +5,10 @@ from pages.db_page import DB_Page
 
 db_page = DB_Page()
 
-def test_get():
-    # requests.get(DB_Page.base_url + DB_Page.db, headers=DB_Page.headers)
-    response = db_page.get_response()
-    print(response.text)
+def test_ui():
+    db_page.open()
+
+# def test_get():
+#     # requests.get(DB_Page.base_url + DB_Page.db, headers=DB_Page.headers)
+#     response = db_page.get_db_structure()
+#     print(response.text)
