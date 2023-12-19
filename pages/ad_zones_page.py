da95e775837210d2ad22e50f922dcb92d1931d2c
tigtrop <fredbios33@gmail.com>
2023-12-19 23:12:33 +0200

    test are updated

--- pages/ad_zones_page.py
+++ pages/ad_zones_page.py
@@ -1,6 +1,17 @@
 import requests
 from hamcrest import *
 from pages.db_page import DB_Page
+from functools import wraps
+def check_status_code(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        response = func(*args, **kwargs)
+
+        assert_that(response.status_code, any_of(equal_to(201), equal_to(200)))
+
+        return response
+
+    return wrapper
 class Ad_zones_page(DB_Page):
 
     ad_zones_endpoint = 'IlyaKnysh/fake_db/ad_zones?'
@@ -11,6 +22,7 @@ class Ad_zones_page(DB_Page):
         for elem in json_page:
             assert_that(list(elem.keys()), equal_to(['id', 'zone', 'type']))
 
+    @check_status_code
     def post_new_zone(self):
         obj = {
                 "id": 11111,
@@ -19,5 +31,5 @@ class Ad_zones_page(DB_Page):
                 }
 
         response = requests.post(self.base_url + self.ad_zones_endpoint, headers=self.headers, json=obj)
-        assert_that(response.status_code, equal_to(201))
+        return response
 
